e725b479fef4bc4129a55029383bbea3b1e7e50c
yesdani <yesdaniece@gmail.com>
2025-05-01 15:41:00 +0100

    username

--- catalogue.sh
+++ catalogue.sh
@@ -1,114 +1,210 @@
 #!/bin/bash
-
+# Set log file
 DATE=$(date +%F)
-SCRIPT_NAME=$0
+SCRIPT_NAME=$(basename "$0") 
 LOGDIR=/tmp
 LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
 
+# Colors for output
 R="\e[31m"
 G="\e[32m"
-N="\e[0m" 
-#Y="\e[33m"
+N="\e[0m"
 
-if [ $USERID -ne 0 ];
-
-then 
-  echo -e " $R Error::: Please run the script with root access $N "
+# Check for root access
+if [ $USERID -ne 0 ]; then 
+  echo -e "${R}Error::: Please run the script with root access${N}"
   exit 1
-
 fi
 
+# Validation function
 VALIDATE(){
-   if [ "$1" -ne 0 ];
-      then
-        echo -e " $2....$R Failure $N"
-        exit 1
-      else 
-      echo -e "   $2 .... $G Success $N"
+   if [ "$1" -ne 0 ]; then
+      echo -e "$2 .... ${R}Failure${N}"
+      exit 1
+   else 
+      echo -e "$2 .... ${G}Success${N}"
    fi
 }
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+# Variables
+roboshop="roboshop"
+directory="/app"
 
-VALIDATE $? 'Setting up Nodesource'
+# NodeJS Installation
+echo -e "\nInstalling NodeJS..."
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+VALIDATE $? "Setting up Nodesource"
 
 yum install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing NodeJS"
 
-VALIDATE $? 'Installig NodeJS'
-
-echo "Please enter username"
-read roboshop
-
-id "$roboshop" 
+# User creation
+id "$roboshop" &>>$LOGFILE
 if [ $? -eq 0 ]; then
-  echo "User '$roboshop' exists."
-  exit 1
+  echo "User '$roboshop' already exists." | tee -a $LOGFILE
 else
-  echo "User '$roboshop' does not exist so adding user."
-  sudo useradd $roboshop
+  useradd $roboshop &>>$LOGFILE
+  VALIDATE $? "Adding user '$roboshop'"
+  echo "User '$roboshop' added." | tee -a $LOGFILE
 fi
-echo "username added is:"$roboshop
 
-echo "Enter Directory name"
-read directory
-# Check if the directory already exists
+# App directory setup
 if [ -d "$directory" ]; then
-  echo "The directory '$directory' already exists."
- 
+  echo "Directory '$directory' already exists." | tee -a $LOGFILE
 else
-  # If it doesn't exist, create the directory
-  sudo mkdir -p "$directory"
-
-  echo "Directory created: $directory"
- 
+  mkdir -p "$directory" &>>$LOGFILE
+  VALIDATE $? "Creating directory '$directory'"
 fi
 
-
+# Deploy catalogue app
+echo -e "\nDeploying catalogue application..."
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+VALIDATE $? "Downloading catalogue artifact"
 
-VALIDATE $? 'Downloading Catalogue Artifact'
-mkdir -p /app
-cd /app &>>$LOGFILE
-VALIDATE $? 'moving to /app directory'
-
-#cd /app&>>$LOGFILE
-
-#VALIDATE $? 'moving to /app directory'
+cd "$directory" &>>$LOGFILE
+VALIDATE $? "Moving to $directory"
 
 unzip /tmp/catalogue.zip &>>$LOGFILE
-
-VALIDATE $? 'Unzipping catalogue'
+VALIDATE $? "Unzipping catalogue"
 
 npm install &>>$LOGFILE
-
-VALIDATE $? 'Installing dependencies'
+VALIDATE $? "Installing NodeJS dependencies"
 
 cp /home/centos/Roboshop-shell-tf/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
-
-VALIDATE $? 'copying catalogue.service'
+VALIDATE $? "Copying catalogue.service"
 
 systemctl daemon-reload &>>$LOGFILE
-
-VALIDATE $? 'daemon reloading'
+VALIDATE $? "Reloading systemd"
 
 systemctl enable catalogue &>>$LOGFILE
-
-VALIDATE $? 'enabling catalogue'
+VALIDATE $? "Enabling catalogue service"
 
 systemctl start catalogue &>>$LOGFILE
+VALIDATE $? "Starting catalogue service"
 
-VALIDATE $? 'enabling catalogue'
-
+# MongoDB setup
+echo -e "\nSetting up MongoDB client..."
 cp /home/centos/Roboshop-shell-tf/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-
-VALIDATE $? 'copying mongorepo'
+VALIDATE $? "Copying mongo.repo"
 
 yum install mongodb-org-shell -y &>>$LOGFILE
-
-VALIDATE $? 'Installing mongo client'
-
+VALIDATE $? "Installing MongoDB client"
 
 mongo --host mongodb.joindevops.top </app/schema/catalogue.js &>>$LOGFILE
+VALIDATE $? "Loading catalogue schema"
 
-VALIDATE $? 'Loading catalogue data.'
+echo -e "\n${G}All tasks completed successfully!${N}"
+
+# DATE=$(date +%F)
+# SCRIPT_NAME=$0
+# LOGDIR=/tmp
+# LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+# USERID=$(id -u)
+
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m" 
+# #Y="\e[33m"
+
+# if [ $USERID -ne 0 ];
+
+# then 
+#   echo -e " $R Error::: Please run the script with root access $N "
+#   exit 1
+
+# fi
+
+# VALIDATE(){
+#    if [ "$1" -ne 0 ];
+#       then
+#         echo -e " $2....$R Failure $N"
+#         exit 1
+#       else 
+#       echo -e "   $2 .... $G Success $N"
+#    fi
+# }
+
+# curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+# VALIDATE $? 'Setting up Nodesource'
+
+# yum install nodejs -y &>>$LOGFILE
+
+# VALIDATE $? 'Installig NodeJS'
+
+# echo "Please enter username"
+# read roboshop
+
+# id "$roboshop" 
+# if [ $? -eq 0 ]; then
+#   echo "User '$roboshop' exists."
+#   exit 1
+# else
+#   echo "User '$roboshop' does not exist so adding user."
+#   sudo useradd $roboshop
+# fi
+# echo "username added is:"$roboshop
+
+# echo "Enter Directory name"
+# read directory
+# # Check if the directory already exists
+# if [ -d "$directory" ]; then
+#   echo "The directory '$directory' already exists."
+ 
+# else
+#   # If it doesn't exist, create the directory
+#   sudo mkdir -p "$directory"
+
+#   echo "Directory created: $directory"
+ 
+# fi
+
+
+# curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+
+# VALIDATE $? 'Downloading Catalogue Artifact'
+# mkdir -p /app
+# cd /app &>>$LOGFILE
+# VALIDATE $? 'moving to /app directory'
+
+# #cd /app&>>$LOGFILE
+
+# #VALIDATE $? 'moving to /app directory'
+
+# unzip /tmp/catalogue.zip &>>$LOGFILE
+
+# VALIDATE $? 'Unzipping catalogue'
+
+# npm install &>>$LOGFILE
+
+# VALIDATE $? 'Installing dependencies'
+
+# cp /home/centos/Roboshop-shell-tf/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+
+# VALIDATE $? 'copying catalogue.service'
+
+# systemctl daemon-reload &>>$LOGFILE
+
+# VALIDATE $? 'daemon reloading'
+
+# systemctl enable catalogue &>>$LOGFILE
+
+# VALIDATE $? 'enabling catalogue'
+
+# systemctl start catalogue &>>$LOGFILE
+
+# VALIDATE $? 'enabling catalogue'
+
+# cp /home/centos/Roboshop-shell-tf/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+# VALIDATE $? 'copying mongorepo'
+
+# yum install mongodb-org-shell -y &>>$LOGFILE
+
+# VALIDATE $? 'Installing mongo client'
+
+
+# mongo --host mongodb.joindevops.top </app/schema/catalogue.js &>>$LOGFILE
+
+# VALIDATE $? 'Loading catalogue data.'
